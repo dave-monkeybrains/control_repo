2b32b9502f72a2676ab36805e054d823dbd1e1d1
dave-monkeybrains <6506671+dave-monkeybrains@users.noreply.github.com>
2020-08-04 09:15:14 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,25 +1,34 @@
 class minecraft {
+  $url         = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar'
+  $install_dir =  '/opt/minecraft'
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  
+  file {"${install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar'
+    source => $url,
+    before => Service['minecraft'],
   }
+  
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  
+  file {"${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true',
   }
+  
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => ['Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
   
 }
